4608b10dda84da8de23522043f29d3a27619906d
ashish082003 <aj9172243856@gmail.com>
2024-08-19 08:03:06 +0530

    final

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,9 +6,9 @@ const generateTokenAndSetCookie = (userId, res) => {
 	});
 
 	res.cookie("jwt", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		maxAge: 15 * 24 * 60 * 60 * 1000, // set cookies life span which is 15 days in millisecond
+		httpOnly: true, // prevent cross site scripting (XSS) attacks cross-site scripting attacks
+		sameSite: "strict", //cross site request forgery CSRF attacks cross-site request forgery attacks
 		secure: process.env.NODE_ENV !== "development",
 	});
 };
