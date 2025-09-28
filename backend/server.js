a24e47e6443236382117230f8866d97422b2a19e
ashish082003 <aj9172243856@gmail.com>
2025-09-28 09:15:34 +0530

    csrf issue fix

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import lusca from "lusca";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -18,6 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(lusca.csrf());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
